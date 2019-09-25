c9f95341d6092031dd488f434ffbd53e79d98c67
Rohit Chavan <53335793+rohitchavan-git@users.noreply.github.com>
2019-09-25 13:00:18 +0530

    init project files

---
+++ learn-java-8-stream/src/main/java/com/rohitchavan/stream/learn/java8/stream/LearnJava8StreamApplication.java
@@ -0,0 +1,13 @@
+package com.rohitchavan.stream.learn.java8.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJava8StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJava8StreamApplication.class, args);
+	}
+
+}
