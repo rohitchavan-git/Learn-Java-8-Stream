9a156d2a3e5962662db0b474577d61143454a1cc
rohit <rohit.chavan@dhdigital.co.in>
2019-09-26 12:39:17 +0530

    all Operations with stream code and sql code added

--- learn-java-8-stream/src/test/java/com/rohitchavan/stream/learn/java8/stream/LearnJava8StreamApplicationTests.java
+++ learn-java-8-stream/src/test/java/com/rohitchavan/stream/learn/java8/stream/LearnJava8StreamApplicationTests.java
@@ -1,7 +1,11 @@
 package com.rohitchavan.stream.learn.java8.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -77,40 +81,82 @@ public class LearnJava8StreamApplicationTests {
 		
 		transactionRepo.saveAll(transactions);
 	}
+	
+	/**
+	 * Supply all Transactions
+	 */
+	public Supplier<List<Transaction>> allTranscation=()->{
+		return  transactionRepo.findAll();
+	};
+	
+	/**
+	 * Supply All Traders
+	 */
+	
+	public Supplier<List<Trader>> allTrader=()->{
+		return  tradersRepo.findAll();
+	};
+	
 	/**
 	 * 1.Find all transactions in the year 2011 and sort them by value (small to high).
-	 * SQL:
+	 * SQL:select * from Transactions where year=2011 order by value asc
+	 * 
+	 * 
+	 * Stream().filter  (Logically Similar To) where clause in sql
+	 * 
+	 * Stream().sorted  (Logically Similar To)  Order By in sql
 	 */
+	
 	@Test
 	public void findAllYear2011() {
+		
+		List<Transaction> allTranscations = allTranscation.get();
+		
+		List<Transaction> result = allTranscations.stream().filter(p->p.getYear()==2011)
+					   .sorted(Comparator.comparing(Transaction::getValue))
+					   .collect(Collectors.toList());
 		
 	}
 	
 	
 	/**
 	 * 2.What are all the unique cities where the traders work?
-	 * SQL:
+	 * SQL:select DISTINCT city from trader
+	 * 
+	 * map (logically similar to) sql projection 
 	 */
 	@Test
 	public void allUniqueCity() {
+		List<Trader> allTraders = allTrader.get();
+		List<String> result = allTraders.stream()
+										.map(Trader::getCity)
+										.distinct()
+										.collect(Collectors.toList());
 		
 	}
 	
 	/**
 	 * 3.Find all traders from Nanded and sort them by name.
-	 * SQL:
+	 * SQL:select * FROM trader WHERE city like "nanded" ORDER by name ASC
 	 */
 	@Test
 	public void allTradersNanded() {
-		
+		List<Trader> allTraders = allTrader.get();
+		allTraders.stream().filter(p->p.getCity()
+						   .equalsIgnoreCase("nanded"))
+						   .sorted(Comparator.comparing(Trader::getName))
+						   .collect(Collectors.toList());
 	}
 	/**
-	 * 4.Return a string of all traders’ names sorted alphabetically.
-	 * SQL:
+	 * 4.Return a string of all traders's names sorted alphabetically.
+	 * SQL:select name FROM trader  ORDER by name ASC
 	 */
 	@Test
 	public void allTradersSorted() {
-		
+		List<Trader> allTraders = allTrader.get();
+		String allNameJoined = allTraders.stream().map(p->p.getName())
+		 				   .sorted()
+		 				   .collect(Collectors.joining());
 	}
 	
 	/**
@@ -119,32 +165,58 @@ public class LearnJava8StreamApplicationTests {
 	 */
 	@Test
 	public void anyTradersInOsbad() {
+		List<Transaction> allTranscations = allTranscation.get();
+		boolean anyMatch = allTranscations.stream().map(transcation->transcation.getTrader())
+					   			.anyMatch(trader->trader.getCity().equalsIgnoreCase("Osmanabad"));
+		if(anyMatch) {
+			allTranscations.stream()
+						.map(transaction->transaction.getTrader())
+						.filter(trader->trader.getCity()
+						.equalsIgnoreCase("Osmanabad")).collect(Collectors.toList());
+					
+		}	
 		
 	}
 	/**
 	 * 6.Print all transactions’ values from the traders living in Nanded.
-	 * SQL:
+	 * SQL:select value from transaction WHERE trader_id IN (select id from trader WHERE city like 'nanded');
 	 */
 	@Test
 	public void allTransaction() {
-		
-		
+		List<Transaction> allTranscations = allTranscation.get();
+		allTranscations.stream().filter(transaction->transaction.getTrader()
+								.getCity().equalsIgnoreCase("Nanded"))
+								.map(trader->trader.getValue())
+								.forEach(System.out::println);;
+							
 	}
 	
 	/**
 	 * 7.What’s the highest value of all the transactions?
-	 * SQL:
+	 * SQL:select sum(value) from transaction 
 	 */
 	@Test
 	public void highTransaction() {
+		List<Transaction> allTranscations = allTranscation.get();
 		
+		Optional<Integer> hightValue = allTranscations.stream().map(trancaction->trancaction.getValue()).reduce(Integer::sum);
 	}
 	/**
 	 * 8.Find the transaction with the smallest value.
-	 * SQL:
+	 * SQL:select * from transaction where VALUE=(SELECT min(value) from transaction);
 	 */
 	@Test
 	public void transactionSmall() {
+		List<Transaction> allTranscations = allTranscation.get();
+		Optional<Transaction> smallValue = allTranscations.stream()
+										.reduce((t1,t2)->t1.getValue()>t2.getValue()?t2:t1);
+		
+		/**
+		 * another way
+		 */
+		Optional<Transaction> smallestTransaction =
+				allTranscations.stream()
+				.min(Comparator.comparing(Transaction::getValue));
 		
 	}
 	
